62c464313457d83b21f15fdc9cf13d4899a362be
lukebowyer22 <lukebowyer22@gmail.com>
2025-03-27 12:36:04 -0400

    Update nutritionTrackerRoutes.js

--- routes/nutritionTrackerRoutes.js
+++ routes/nutritionTrackerRoutes.js
@@ -6,7 +6,6 @@ const router = Router();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Serve nutrientTracker.html at the root route
 router.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'nutrientTracker.html'));
 });
