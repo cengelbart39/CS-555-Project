db940d844b096bc5bb5a343f8c8b58df1e349a88
lukebowyer22 <lukebowyer22@gmail.com>
2025-03-27 13:44:08 -0400

    Update Index.js

--- routes/Index.js
+++ routes/Index.js
@@ -1,7 +1,7 @@
 import tempRoutes from './nutritionTrackerRoutes.js';
 
 const constructorMethod = (app) => {
-  app.use('/', tempRoutes);
+  app.use('/', nutritionTrackerRoutes);
 
   app.use('*', (req, res) => {
     res.status(404).send('Page not found');
