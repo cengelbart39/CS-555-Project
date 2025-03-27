79fec387ec6da8d343bc24692a33f38428bec32c
lukebowyer22 <lukebowyer22@gmail.com>
2025-03-27 12:35:26 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+import configRoutes from './routes/index.js';
 
 const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(express.json());
 
-// configRoutes(app);
+configRoutes(app);
+
+app.use(express.static(__dirname));
+
 
 app.listen(3000, () => {
   console.log("We've now got a server!");
